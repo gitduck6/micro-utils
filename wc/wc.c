156df5e7896ec6cc8145c6c61e3b2423fee710b3
gitduck6 <warmsot19@gmail.com>
2026-05-27 12:06:11 +0400

    declared functions and minor changes

--- wc/wc.c
+++ wc/wc.c
@@ -17,6 +17,11 @@ int lflag = 0;
 int wflag = 0;
 int Lflag = 0;
 
+int get_bytes(FILE *fd);
+int get_chars(FILE *fd);
+int get_lines(FILE *fd);
+int get_words(FILE *fd);
+int get_longest_line(FILE *fd);
 
 int main (int argc, char **argv)
 {
@@ -26,26 +31,30 @@ int main (int argc, char **argv)
         switch (c)
         {
         case 'c':
-            
+            cflag = 1;
             break;
         case 'm':
-            
+            mflag = 1;
             break;
             
         case 'l':
-            
+            lflag = 1;
             break;
             
         case 'w':
-            
+            wflag = 1;
             break;
             
         case 'L':
-            
+            Lflag = 1;
             break;
             
         default:
+            wflag = cflag = lflag = 1;
             break;
         }
     }
+
+    argc -= optind;
+    argv += optind;
 }
