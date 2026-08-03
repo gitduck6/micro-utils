3eec97c5623db825ca86dba3d17b0a9c363cc9c4
gitduck6 <warmsot19@gmail.com>
2026-08-03 18:32:16 +0400

    Added functions

--- mkdir/mkdir.c
+++ mkdir/mkdir.c
@@ -2,10 +2,16 @@
 
 #include <stdio.h>
 #include <unistd.h>
+#include <sys/stat.h>
+
+
+mode_t arr_to_mode(char *arr, char * status);
+int is_octal(char c);
 
 int main(int argc, char ** argv)
 {
     int pflag = 0, vflag = 0;
+
 
     int c;
     while ((c = getopt(argc, argv, "pvm:")))
@@ -13,18 +19,53 @@ int main(int argc, char ** argv)
         switch (c)
         {
             case 'p':
+                pflag = 1;
                 break;
             case 'v':
+                vflag = 1;
                 break;
             case 'm':
                 break;
             default:
-                fprintf(stderr, "Usage : %s -[pv] [directories]", argv[0]);
-                fprintf(stderr, "Usage : %s -m [mode] [directories]", argv[0]);
+                fprintf(stderr, "Usage : %s -[pv] [directories]\n", argv[0]);
+                fprintf(stderr, "Usage : %s -[pv] -m [mode] [directories]\n", argv[0]);
                 return 1;
                 break;
         }
     }
 
     return 0;
+}
+
+int is_octal(char c)
+{
+    if ((c >= '0') && (c <= '7'))
+        return 1;
+
+    return 0;
+}
+
+
+mode_t arr_to_mode(char *arr, char * status)
+{
+    mode_t mode = 0;
+    while (*arr)
+    {
+        if (is_octal(*arr))
+        {
+            mode *= 8;
+            mode += (*arr - '0');
+        }
+        else
+        {
+            *status = 1; // 1 status incase of fail
+            return mode;
+        }
+        arr++;
+    }
+
+    if (mode > 0777)
+        *status = 2;
+
+    return mode;
 }
